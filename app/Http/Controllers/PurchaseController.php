364f9d91fe438183f54b2f8dd84ecdf3b20916f5
kmori <k.morishita1028@outlook.jp>
2022-11-03 00:43:21 +0900

    feat: 購入画面作成、検索機能追加

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdatePurchaseRequest;
 use App\Models\Customer;
 use App\Models\Item;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseController extends Controller
 {
@@ -48,18 +49,30 @@ class PurchaseController extends Controller
     public function store(StorePurchaseRequest $request)
     {
         // dd($request);
-        $purchase = Purchase::create([
-            'customer_id' => $request->customer_id,
-            'status' => $request->status
-        ]);
 
-        foreach ($request->items as $item) {
-            $purchase->items()->attach($purchase->id, [
-                'item_id' => $item['id'],
-                'quantity' => $item['quantity']
+        DB::beginTransaction();
+
+        try{
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status
             ]);
+
+            foreach($request->items as $item){
+                $purchase->items()->attach($purchase->id, [
+                    'item_id' => $item['id'],
+                    'quantity' => $item['quantity']
+                ]);
+            }
+
+            DB::commit();
+
+            return to_route('dashboard');
+
+        } catch(\Exception $e){
+            DB::rollback();
         }
-        return to_route('dashboard');
+
     }
 
     /**
