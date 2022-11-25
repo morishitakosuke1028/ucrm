abab804d7df1683e8bd1d3044fc67419ee441fb1
kmori <k.morishita1028@outlook.jp>
2022-11-26 01:04:07 +0900

    feat: postテーブル作成

--- database/migrations/2022_11_26_004203_create_posts_table.php
+++ database/migrations/2022_11_26_004203_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade');;
+            $table->string('title');
+            $table->string('content');
             $table->timestamps();
         });
     }
