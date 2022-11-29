cc100124acae780d8edfad087a003ae829fbd140
kmori <k.morishita1028@outlook.jp>
2022-11-29 22:27:57 +0900

    feat: 分析検証途中

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -22,16 +22,32 @@ class AnalysisController extends Controller
 
         // dd($period);
 
-         $subQuery = Order::betweenDate($startDate, $endDate)
-        ->where('status', true)
-        ->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as totalPerPurchase,
-        DATE_FORMAT(created_at, "%Y%m%d") as date');
+        //  $subQuery = Order::betweenDate($startDate, $endDate)
+        // ->where('status', true)
+        // ->groupBy('id')
+        // ->selectRaw('id, sum(subtotal) as totalPerPurchase,
+        // DATE_FORMAT(created_at, "%Y%m%d") as date');
 
-        $data = DB::table($subQuery)
-        ->groupBy('date')
-        ->selectRaw('date, sum(totalPerPurchase) as total' )
-        ->get();
+        // $data = DB::table($subQuery)
+        // ->groupBy('date')
+        // ->selectRaw('date, sum(totalPerPurchase) as total' )
+        // ->get();
+
+        // RFM分析
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, customer_id, customer_name,
+        SUM(subtotal) as totalPerPurchase, created_at');
+
+        // 2. 会員毎にまとめて最終購入日、回数、合計金額を取得
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id, customer_name,
+        max(created_at) as recentDate,
+        datediff(now(), max(created_at)) as recency,
+        count(customer_id) as frequency,
+        sum(totalPerPurchase) as monetary');
 
         // dd($data);
         return Inertia::render('Analysis');
