0486d728abf57fdbe3f806dd77202ffeaabd19ee
kmori <k.morishita1028@outlook.jp>
2023-01-02 12:19:07 +0900

    feat: storeリクエストのバリデーション追加

--- app/Http/Requests/StoreRecruitRequest.php
+++ app/Http/Requests/StoreRecruitRequest.php
@@ -13,7 +13,7 @@ class StoreRecruitRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StoreRecruitRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'max:50'],
+            'email' => ['required', 'string', 'email', 'max:255',],
+            'content' => ['required',],
         ];
     }
 }
