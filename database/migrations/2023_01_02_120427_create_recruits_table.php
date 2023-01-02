833ef4f445d4e51204546bd028f5e9f5d86a454c
kmori <k.morishita1028@outlook.jp>
2023-01-02 12:16:06 +0900

    feat: db設定

--- database/migrations/2023_01_02_120427_create_recruits_table.php
+++ database/migrations/2023_01_02_120427_create_recruits_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('recruits', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('content');
+            $table->string('status');
+            $table->string('member');
             $table->timestamps();
         });
     }
