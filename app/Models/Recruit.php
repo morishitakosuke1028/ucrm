b1e9ecfc0d4dafeb648d34e698dc46b3de2cfeb9
kmori <k.morishita1028@outlook.jp>
2023-01-02 12:17:51 +0900

    feat: モデル設定

--- app/Models/Recruit.php
+++ app/Models/Recruit.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Recruit extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'content',
+        'status',
+        'member',
+    ];
 }
